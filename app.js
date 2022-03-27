e442eb743924bbf928dc94886f77bd3213161681
Ben Guetta <95125332+bn-jo@users.noreply.github.com>
2022-03-27 23:49:39 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+
     const squares = document.querySelectorAll('.grid div')
     const scoreDisplay = document.querySelector('span')
     const startBtn = document.querySelector('.start')
+
+    const up = document.getElementById('up')
+    const down = document.getElementById('down')
+    const left = document.getElementById('left')
+    const right = document.getElementById('right')
+
 
     const width = 10
     let currentIndex = 0
@@ -12,6 +20,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let speed = 0.9
     let intervalTime = 0
     let interval = 0
+
+
 
     function startGame() {
         currentSnake.forEach(index => squares[index].classList.remove('snake'))
@@ -46,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
         squares[tail].classList.remove('snake')
         currentSnake.unshift(currentSnake[0] + direction)
 
-        //deals with snake getting apple
+        //deals with snake eat apple
         if (squares[currentSnake[0]].classList.contains('apple')) {
             squares[currentSnake[0]].classList.remove('apple')
             squares[tail].classList.add('snake')
@@ -74,9 +84,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    //assign functions to keycodes
+
+
+
     function control(e) {
+
+
+
+
+
+
+
         squares[currentIndex].classList.remove('snake')
+
+
+
+
+
+
+
+
 
         if (e.keyCode === 39) {
             direction = 1
@@ -89,6 +116,23 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+
+
+
+
+
     document.addEventListener('keyup', control)
+
+
     startBtn.addEventListener('click', startGame)
+
+
+
+    up.addEventListener('click', () => { direction = -width })
+    down.addEventListener('click', () => { direction = +width })
+    left.addEventListener('click', () => { direction = -1 })
+    right.addEventListener('click', () => { direction = +1 })
+
+
+
 })
